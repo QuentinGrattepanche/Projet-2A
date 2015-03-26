c7fd82351d59ceb8ad6e668bd1bef31dbd9d277c
AliceHH <achache.alice@gmail.com>
2015-03-26 13:12:41 +0100

    Ajout du traitement de la question simple (avec plusieurs réponses valides) dans MOODLE-> JAVA

--- GestionnaireMoodleJava.java
+++ GestionnaireMoodleJava.java
@@ -6,8 +6,10 @@ public class GestionnaireMoodleJava extends GestionnaireLecture{
 	//résultats de notre parsing 
 	//[changement heredite]private Quizz quiz; 
 	private Question questionEnCoursDeTraitement; 
+	//type de la question en cours de traitement
+	private String type;
 	//flags nous indiquant la position du parseur 
-	private boolean inQuiz, inQuestion, inNom, inEnonce,inText, inReponse;
+	private boolean inQuiz, inQuestion, inNom, inEnonce,inText, inReponse, inReponsePrincipale, a_deja_eu_reponse, inReponseAlternative;
 	//buffer nous permettant de récupérer les données  
 	private StringBuffer buffer; 
 	//nom du fichier final
@@ -15,7 +17,7 @@ public class GestionnaireMoodleJava extends GestionnaireLecture{
 
 	// simple constructeur 
 	public GestionnaireMoodleJava(){ 
-		super(); 
+		super();
 		//[changement heredite]this.nomFichierFinal = "";
 	} 
 
@@ -27,62 +29,57 @@ public class GestionnaireMoodleJava extends GestionnaireLecture{
 		//[Test pour l'affichage]System.out.println(qName);
 		//[Test pour l'affichage]System.out.println(attributes.getValue("type"));
 
+		switch (qName) {
 		//Si la balise est "quiz" :
-		if(qName.equals("quiz")){ 
+		case "quiz":
 			//on crée un quizz et on marque que l'on est en train de le traiter
 			quiz = new Quizz();  
 			inQuiz = true; 
 			//[Test pour l'affichage]System.out.println("on réussi à passer la balise \"quiz\"");
-		}
+			break;
 
-		//Si la balise n'est pas "question" et qu'aucune question est en cours de traitement : on ne fait rien 
-		//(il peut y avoir des balises intermédiaires non utilisées dans notre fichier Moodle)
-		else if(!(qName.equals("question")) && (inQuestion==false)){
-			//[Test pour l'affichage]System.out.println("on saute une balise ouvrante");
-		}
-
-		//Si la balise est  "question" :
-		else if(qName.equals("question")){ 
+			//Si la balise est  "question" :
+		case "question":
 			//[Test pour l'affichage]System.out.println("on réussi à passer la balise \"question\"");
 			//[Test pour l'affichage]System.out.println(attributes.getValue("type"));
 
-			//On determine le type de question
+			// On signale que l on est dans une question :
+			inQuestion = true;
+			type = attributes.getValue("type");
 
-			switch (attributes.getValue("type")) {
+			//On determine le type de question
+			switch (type) {
 
 			case "multichoice" :
 				questionEnCoursDeTraitement = new QuestionMultiChoix();
-				inQuestion = true;
 				//[Test pour l'affichage]System.out.println("on crée une question à choix multiple");
 				break;
-				
+
 			case "truefalse" :
 				questionEnCoursDeTraitement = new QuestionVraiFaux();
-				inQuestion = true;
 				//[Test pour l'affichage]System.out.println("on crée une question Vrai Faux");
 				break;
-				
+
 			case "shortanswer" :
 				questionEnCoursDeTraitement = new QuestionSimple();
-				inQuestion = true;
 				//[Test pour l'affichage]System.out.println("on crée une question à réponse courte");
 				break;
-				
+
 			case "numerical" :
 				break;
-			
+
 			case "matching" :
 				break;
-				
+
 			case "cloze" :
 				break;
-				
+
 			case "essay" :
 				break;
-				
+
 			case "description" :
 				break;
-			}			
+			}
 
 			//TODO Si on veut rajouter un identifiant (A REVOIR)
 			//try{ 
@@ -91,87 +88,166 @@ public class GestionnaireMoodleJava extends GestionnaireLecture{
 			//catch(Exception e){ 
 			//erreur, le contenu de id n'est pas un entier 
 			//throw new SAXException(e); } 
-			//inQuestion = true; 
+			//inQuestion = true;
+
+			break;
+
+		default:
+			//Si la balise n'est pas "question" et qu'aucune question est en cours de traitement : on ne fait rien 
+			//(il peut y avoir des balises intermédiaires non utilisées dans notre fichier Moodle)
+			if(!inQuestion){
+				//[Test pour l'affichage]System.out.println("on saute une balise ouvrante");
+			}
+
+			// sinon, on est deja dans une question.
+			else {
+				buffer = new StringBuffer(); 
+				//[Test pour l'affichage]System.out.println("qName courant : "+qName);
+
+				// le nom et l enonce de la question est le meme quel que soit le type.
+				switch (qName) {
+				case "name" :
+					inNom = true;  //on marque que l'on va lire ensuite dans le fichier xml le nom de la question
+					break;
+
+				case "questiontext" : 
+					inEnonce = true; 
+					break;
+
+				default : //pour les autres champs, cela depend du type de question :
+					switch (type) {
+					case "multichoice" :
+						break;
+
+					case "truefalse" :
+						switch (qName) {
+						case "answer" :
+							//[Test pour l'affichage]System.out.println("La fraction associée à la réponse suivante est : "+ attributes.getValue("fraction"));
+							if(attributes.getValue("fraction").equals("100")){ //si la réponse est vrai
+								inReponse = true; 
+							}
+							break;
+						case "text" :
+							inText = true; 
+							break;
+							//si la balise dans la question n'est aucune de celles que l'on traite il ne se passe rien
+						}
+						break;
+
+					case "shortanswer" :
+						switch (qName) {
+						case "answer" :
+							if(attributes.getValue("fraction").equals("100")){//si la réponse est vrai
+								if(!a_deja_eu_reponse){
+									inReponsePrincipale = true; 
+									a_deja_eu_reponse = true;
+								}
+								else {
+									inReponseAlternative = true;
+								}
+							}
+							break;
+						case "text" :
+							inText = true; 
+							break;
+							//si la balise dans la question n'est aucune de celles que l'on traite il ne se passe rien
+						}
+						break;
+
+					case "numerical" :
+						break;
+
+					case "matching" :
+						break;
+
+					case "cloze" :
+						break;
+
+					case "essay" :
+						break;
+
+					case "description" :
+						break;
+					}
+					break;
+				}
+			}
 		}
+	}
 
-		//On traite les balises qui nous intéressent dans la question :
-		else { 
-			buffer = new StringBuffer(); 
-			//[Test pour l'affichage]System.out.println(attributes.getValue("fraction"));
-			//[Test pour l'affichage]System.out.println(qName);
-			if(qName.equals("name")){ 
-				inNom = true; } //on marque que l'on va lire ensuite dans le fichier xml le nom de la question
-			else if(qName.equals("questiontext")){ 
-				inEnonce = true; }
-			else if(qName.equals("answer") && (attributes.getValue("fraction").equals("100"))){ //si la réponse est vrai
-				inReponse = true; }
-			else if(qName.equals("answer") && !(attributes.getValue("fraction").equals("100"))){ //si la réponse est fausse
-				inReponse = false; }
-
-			else if(qName.equals("text")){ 
-				inText = true; }
-
-			//si la balise dans la question n'est aucune de celles que l'on traite il ne se passe rien
-			else if(!(qName.equals("name")) && !(qName.equals("questiontext")) &&!(qName.equals("answer"))){}
-
-			else{ 
-				//erreur, on peut lever une exception 
-				throw new SAXException("Balise "+qName+" inconnue."); 
-			} 
-		} 
-	} 
 
 	//méthode lancée lors d'une balise fermante 
 	public void endElement(String uri, String localName, String qName) 
 			throws SAXException{ 
 
-		//Si la balise fermante est "quiz" : on reinitialise le marqueur inquiz
-		if(qName.equals("quiz")){ 
-			inQuiz = false; }
+		switch (qName) {
+		case "quiz" :
+			//Si la balise fermante est "quiz" : on reinitialise le marqueur inquiz
+			inQuiz = false; 
+			break;
 
-		//Si la balise fermante est "question" et que l'on ne traitait pas de question : il ne se passe rien
-		else if(!qName.equals("question") && (inQuestion==false)){
-			//[Test pour l'affichage]System.out.println("on saute une balise fermante");
-		}
-
-		//Si la balise fermante est "question" : on ajoute la question au quizz et on réinitialise
-		else if(qName.equals("question")){ 
+		case "question" :
+			//Si la balise fermante est "question" : on ajoute la question au quizz et on réinitialise 
 			quiz.getMesQuestions().add(questionEnCoursDeTraitement); 
 			questionEnCoursDeTraitement = null; 
 			inQuestion = false; 
-		}
+			a_deja_eu_reponse = false;
+			break;
 
-		//Si la balise fermante est "name", "questiontext" ou "answer"(avec la réponse juste) : on reinitialise le marqueur associé
-		else if(qName.equals("name")){  
-			inNom = false; }
-		else if(qName.equals("questiontext")){  
-			inEnonce = false; }
-		else if(qName.equals("answer") && inReponse){ 
-			inReponse = false; }
+			//Si la balise fermante est "name", "questiontext" ou "answer"(avec la réponse juste) : on reinitialise le marqueur associé
+		case "name" :  
+			inNom = false; 
+			break;
+		case "questiontext" :  
+			inEnonce = false; 
+			break;
+		case "answer" :
+			if(inReponse){ 
+				inReponse = false; 
+			}
+			if(inReponsePrincipale){ 
+				inReponsePrincipale = false; 
+			}
+			if(inReponseAlternative){
+				inReponseAlternative = false;
+			}
+			break;
 
-		//Si la balise fermante est "text" : on récupère le texte du buffer et on le met dans l'attribut de la question qui correspond :
-		else if(qName.equals("text")){ 
+			//Si la balise fermante est "text" : on récupère le texte du buffer et on le met dans l'attribut de la question qui correspond :
+		case "text" : 
 			if (inNom){
-				questionEnCoursDeTraitement.setNom(buffer.toString()); }
+				questionEnCoursDeTraitement.setNom(buffer.toString());
+				//[Test pour l'affichage]System.out.println("Nom de la question : "+buffer.toString());
+				
+			}
 			if (inEnonce){
-				questionEnCoursDeTraitement.setEnonce(buffer.toString()); }
+				questionEnCoursDeTraitement.setEnonce(buffer.toString()); 
+				//[Test pour l'affichage]System.out.println("Enoncé de la question : "+buffer.toString());
+			}
 			if (inReponse){
-				//[Test pour l'affichage]System.out.println(buffer.toString());
+				//[Test pour l'affichage]System.out.println("Réponse : "+buffer.toString());
 				questionEnCoursDeTraitement.setReponse(buffer.toString()); 
-				inReponse = false;}
+			}
+			if (inReponsePrincipale){
+				//[Test pour l'affichage]System.out.println("Réponse principale : "+buffer.toString());
+				questionEnCoursDeTraitement.setReponse(buffer.toString()); 
+			}
+			if (inReponseAlternative){
+				//[Test pour l'affichage]System.out.println("Réponse alternative : "+ buffer.toString());
+				((QuestionSimple) questionEnCoursDeTraitement).ajouterReponseAlternative(buffer.toString()); 
+			}
 			//on réinitialise le buffer et le marqueur "inText"
 			buffer = null;
 			inText = false;
+			inReponse = false;
+			inReponsePrincipale = false;
+			inReponseAlternative = false;
+
+		default :
+			//Si la balise fermante n est pas une de celle que l on devait traiter : il ne se passe rien
+			//[Test pour l'affichage]System.out.println("on ferme la balise");
 		}
-
-		//Si la balise fermante n'est aucune de celle que l'on devait traiter : il ne se passe rien
-		else if(!(qName.equals("name")) && !(qName.equals("questiontext")) ){}
-
-		else{ 
-			//erreur, on peut lever une exception 
-			throw new SAXException("Balise "+qName+" inconnue."); 
-		}           
-	} 
+	}
 
 	//détection de caractères 
 	public void characters(char[] ch,int start, int length) 
--- QuestionSimple.java
+++ QuestionSimple.java
@@ -1,7 +1,16 @@
+import java.util.Vector;
+
 public class QuestionSimple extends Question{
+	private Vector<String> reponsesAlternatives;
 	public QuestionSimple(){
 		super();
 		this.setType("Multichoix");
+		this.reponsesAlternatives = new Vector<String>();
+	}
+	String ajouterReponseAlternative(String reponse) {
+		reponsesAlternatives.add(reponse);
+		return reponse;
 	}
 }
+
 
